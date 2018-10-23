00e4c5942b0ca99a047a5861e233b79ef0cc9825
Nathan Richmond <noah.salazar@tutanota.com>
2018-10-23 20:28:00 +0200

    Update upload

--- Python sucht Hamster/src/actions/Collision.java
+++ Python sucht Hamster/src/actions/Collision.java
@@ -2,21 +2,95 @@ package actions;
 
 import chars.Enemy;
 import chars.Player;
+import clocks.Korn_Creation;
+import clocks.Wall_Creation;
+import game.Gamestate;
+import game.Gamestate_e;
+import game.Korn;
+import game.Wall;
 import gui.Button;
 
 public class Collision {
 
+	private static Player p;
+	private static Enemy e;
+
+	public Collision() {
+		p = Main.p;
+		e = Main.e;
+	}
+
+	/*
+	 * Check collision with any button
+	 */
 	public static boolean cButton(Button button, int x, int y) {
 		return (x < button.getX() + button.getWidth() && x > button.getX() && y < button.getY() + button.getHeight()
 				&& y > button.getY());
 	}
 
+	/*
+	 * Check collision of Enemy and Player
+	 */
 	public static boolean cEnemyPlayer() {
-		if (Player.getX() == Enemy.getX() && Player.getY() == Enemy.getY()) { // both have the same coordinates
-			return true;
-		} else {
-			return false;
+		p = Main.p;
+		e = Main.e;
+		boolean flag = false;
+		if (Gamestate.state == Gamestate_e.ingame) {
+			if (p.getX() == e.getX() && p.getY() == e.getY()) {
+				flag = true;
+			} else {
+				flag = false;
+			}
 		}
+		return flag;
+	}
+
+	/*
+	 * Check collision with any Wall
+	 */
+	public static boolean cWall(int x, int y) {
+		boolean flag = false;
+		for (Wall w : Wall_Creation.walls) {
+			if (x == w.getX() && y == w.getY()) {
+				flag = true;
+				break;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+
+	/*
+	 * Check collision with any Korn
+	 */
+	public static boolean cKorn(int x, int y) {
+		boolean flag = false;
+		for (Korn k : Korn_Creation.koerner) {
+			if (x == k.getX() && y == k.getY()) {
+				flag = true;
+				break;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+
+	/*
+	 * Check collision with Player or Enemy. Used for determining valid spawns of
+	 * Wall and Korn
+	 */
+	public static boolean cPlayerOrEnemy(int x, int y) {
+		p = Main.p;
+		e = Main.e;
+		boolean flag = false;
+		if ((x == p.getX() && y == p.getY()) || (x == e.getX() && y == e.getY())) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+		return flag;
 	}
 
 }
--- Python sucht Hamster/src/actions/MousePressed.java
+++ Python sucht Hamster/src/actions/MousePressed.java
@@ -7,28 +7,48 @@ import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 
 public class MousePressed implements EventHandler<MouseEvent> {
-    @Override
-    public void handle(MouseEvent e) {
+	@Override
+	public void handle(MouseEvent e) {
 
-
-        switch (Gamestate.state) {
-            case ingame:
-                //Player.isShooting = true;
-                break;
-            case pause:
-                if (Collision.cButton(Gui.buttons[0], (int) e.getX(), (int) e.getY())) {
-                    Gamestate.state = Gamestate_e.ingame;
-                }
-                if (Collision.cButton(Gui.buttons[1], (int) e.getX(), (int) e.getY())) {
-                    Gamestate.state = Gamestate_e.pause;
-                }
-                if (Collision.cButton(Gui.buttons[2], (int) e.getX(), (int) e.getY())) {
-                    System.exit(0);
-                }
-                break;
+		switch (Gamestate.state) {
+		case pause:
+			if (Collision.cButton(Gui.pausebuttons[0], (int) e.getX(), (int) e.getY())) {
+				Gamestate.state = Gamestate_e.ingame;
+			}
+			if (Collision.cButton(Gui.pausebuttons[1], (int) e.getX(), (int) e.getY())) {
+				Gamestate.state = Gamestate_e.startmenu;
+			}
+			if (Collision.cButton(Gui.pausebuttons[2], (int) e.getX(), (int) e.getY())) {
+				System.exit(0);
+			}
+			break;
+		case startmenu:
+			if (Collision.cButton(Gui.startmenubuttons[0], (int) e.getX(), (int) e.getY())) {
+				Main.startNewGame();
+			}
+			if (Collision.cButton(Gui.startmenubuttons[1], (int) e.getX(), (int) e.getY())) {
+				System.exit(0);
+			}
+			break;
+		case victory:
+			if (Collision.cButton(Gui.gameendbuttons[0], (int) e.getX(), (int) e.getY())) {
+				Gamestate.state = Gamestate_e.startmenu;
+			}
+			if (Collision.cButton(Gui.gameendbuttons[1], (int) e.getX(), (int) e.getY())) {
+				System.exit(0);
+			}
+			break;
+		case defeat:
+			if (Collision.cButton(Gui.gameendbuttons[0], (int) e.getX(), (int) e.getY())) {
+				Gamestate.state = Gamestate_e.startmenu;
+			}
+			if (Collision.cButton(Gui.gameendbuttons[1], (int) e.getX(), (int) e.getY())) {
+				System.exit(0);
+			}
+			break;
 		default:
 			break;
-        }
+		}
 
-    }
+	}
 }
